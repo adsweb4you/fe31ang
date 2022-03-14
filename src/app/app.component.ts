67b77f7c9fdcda28a75f105b98874ed15a25f9e1
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-03-14 21:33:26 +0400

    ang31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component , ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // template:`<h1> {{title}} </h1>`,
+  // styles:['h1{color:green}'],
+  // encapsulation:ViewEncapsulation.Emulated
 })
 export class AppComponent {
-  title = 'fe31ang';
+  title =  6665464;
+  y = 2353;
+  shows = false;
+  color = "assets/logo.png"
+
+  calcu(u:any){
+   return u;
+  }
+
+  changecolor(col:any){
+    this.color = col;
+ 
+  }
+
+  toggle(){
+  this.shows = !this.shows
+  }
+
 }
